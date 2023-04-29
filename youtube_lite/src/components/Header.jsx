eb335c23a0625ea5889af8f3b1ffabf3f43b5420
AMahakud07 <abhishekmahakud007@gmail.com>
2023-04-29 17:48:02 +0530

    started

---
+++ youtube_lite/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React,{useContext,useState} from 'react'
+
+import {Link, useLocation, useNavigate} from "react-router-dom";
+
+import ytLogo from "../images/yt-logo.png";
+import ytLogoMobile from "../images/yt-logo-mobile.png";
+
+import {SlMenu} from "react-icons/sl";
+import {IoIosSearch} from "react-icons/io";
+import {RiVideoAddLine} from "react-icons/ri";
+import {FiBell} from "react-icons/fi";
+import {CgClose} from "react-icons/cg";
+
+import {Context} from "../context/contextApi";
+// import Loader from "../shared/loader"
+
+
+const Header = () => {
+
+  const [searchQuery , setSearchQuery] = useState("");
+
+  const {loading , mobileMenu , setMobileMenu} = useContext(Context)
+
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
