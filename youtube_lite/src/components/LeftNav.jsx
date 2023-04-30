19ddaa3f7b94737296c952689b597f36fa1a4432
AMahakud07 <abhishekmahakud007@gmail.com>
2023-04-30 15:52:09 +0530

    progress

--- youtube_lite/src/components/LeftNav.jsx
+++ youtube_lite/src/components/LeftNav.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { categories } from "../utils/constants";
+import { Context } from "../context/contextApi";
+
 
 const LeftNav = () => {
+  const { selectCatgories, setSelectCategories, mobileMenu } = useContext(Context);
   return (
-    <div>LeftNav</div>
+    <div className='md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all'>
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment>
+              <LeftNavMenuItem
+                text={item.type === "home" ? "Home" : item.name}
+                icon={item.icon}
+                action={() => { }}
+                className={`${selectCatgories === item.name ? "bg-white/[0.15]" : ""}`}
+              />
+              {item.divider && (
+                <hr className='my-5 border-white/[0.2] ' />
+              )}
+            </React.Fragment>
+          )
+        })}
+        <hr className='my-5 border-white/[0.2] ' />
+        <div className="text-white/[0.5] text-[12px]">
+          Clone by : Abhishek Mahakud
+        </div>
+      </div>
+    </div>
   )
 }
 
